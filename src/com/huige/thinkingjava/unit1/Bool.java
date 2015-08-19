84d6bbc9e35c0c280e77460520cc8ca1a8244ea2
ithuige <ithuige@163.com>
2015-08-19 17:58:05 +0800

    2015-8-19 18：00

---
+++ src/com/huige/thinkingjava/unit1/Bool.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.huige.thinkingjava.unit1;
+
+import java.util.Random;
+
+/**
+ * @author huige
+ *
+ *         2015年8月19日 下午5:21:13
+ */
+public class Bool {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int i = rand.nextInt() % 100;
+		int j = rand.nextInt() % 100;
+		prt("i = " + i);
+		prt("j = " + j);
+		prt("i > j is " + (i > j));
+		prt("i < j is " + (i < j));
+		prt("i >= j is " + (i >= j));
+		prt("i <= j is " + (i <= j));
+		prt("i == j is " + (i == j));
+		prt("i != j is " + (i != j));
+		// Treating an int as a boolean is
+		// not legal Java
+		// ! prt("i && j is " + (i && j));
+		// ! prt("i || j is " + (i || j));
+		// ! prt("!i is " + !i);
+		prt("(i < 10) && (j < 10) is " + ((i < 10) && (j < 10)));
+		prt("(i < 10) || (j < 10) is " + ((i < 10) || (j < 10)));
+	}
+
+	static void prt(String s) {
+		System.out.println(s);
+	}
+}
